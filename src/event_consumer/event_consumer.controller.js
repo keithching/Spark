c2786e0804f39428af10a6bdcd6d81000f4f2df6
Keith <keithching91@gmail.com>
2023-02-07 23:11:57 +0900

    fix: import isNumberOfString into event consumer controller

--- src/event_consumer/event_consumer.controller.js
+++ src/event_consumer/event_consumer.controller.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const eventConsumerModel = require("./event_consumer.model");
+const { isNumberOrString } = require("../../util/validation");
 
 router.get("/", async (req, res) => {
   try {
