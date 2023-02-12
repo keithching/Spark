4a7db424830d0ed8808fdf0f834556a99cb0b234
Keith <keithching91@gmail.com>
2023-02-12 23:32:48 +0900

    feat: add PATCH keyword to HTTP header

--- src/server.js
+++ src/server.js
@@ -17,7 +17,10 @@ app.use(cors());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
