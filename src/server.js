5bfcc4b7645ad8012d9ca6b8f0cb123d877281d1
Keith <keithching91@gmail.com>
2023-02-12 23:37:05 +0900

    commented out the app.use res.setHeader

--- src/server.js
+++ src/server.js
@@ -15,15 +15,15 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PATCH, DELETE, OPTIONS"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 
 const PORT = process.env.PORT || 4000;
 
